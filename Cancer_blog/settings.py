bfba58d3a4ea92f34f4646c24f8e683daad32d4f
Rajveer Singh <113203480+Rajveer-03@users.noreply.github.com>
2023-11-15 18:46:31 +0530

    Update settings.py

--- Cancer_blog/settings.py
+++ Cancer_blog/settings.py
@@ -127,7 +127,7 @@ MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'blog/static')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
